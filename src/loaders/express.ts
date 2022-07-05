ce0bf99a19d8eefba34d525ecd1d896fe3059f35
hyunjin <leehj0110@kakao.com>
2022-07-05 15:51:49 +0900

    feat: loader에 baseRouter 추가

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 
+import baseRouter from '@routes/index';
+
 const expressLoader = (app: express.Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+
+  app.use('/api', baseRouter);
 };
 
 export default expressLoader;
