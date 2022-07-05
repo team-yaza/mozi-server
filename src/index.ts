42a26c7aa605db164d4315910dca76e21e1156f4
hyunjin <leehj0110@kakao.com>
2022-07-05 15:19:53 +0900

    feat: mongodb 연결

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-// import loader from './loaders';
 
-// import loader from '@loaders';
+import loader from '@loaders/index';
 
-const startSever = () => {
+const startSever = async () => {
   const app = express();
+
+  await loader(app);
+
   app.listen(3000, () => console.log('server started'));
 };
 
