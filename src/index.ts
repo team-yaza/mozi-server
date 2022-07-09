688de0bf4386aa1378907941f4e484c628b68854
hyunjin <leehj0110@kakao.com>
2022-07-10 01:32:05 +0900

    chore: loader 절대경로 수정

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
-// import loader from './loaders';
 
-// import loader from '@loaders';
+import loader from '@/loaders';
 
 const startSever = () => {
   const app = express();
+
+  loader(app);
+
   app.listen(3000, () => console.log('server started'));
 };
 
