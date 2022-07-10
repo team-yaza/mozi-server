d7b6978016054c771218cecb86d27952c60ca9d4
hyunjin <leehj0110@kakao.com>
2022-07-10 13:04:46 +0900

    refactor: add mongo loader

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -1,10 +1,13 @@
 import 'dotenv/config';
 import express from 'express';
 
-import expressLoader from './express';
+import expressLoader from '@/loaders/express';
+import mongooseLoader from '@/loaders/mongo';
 
-const loader = (app: express.Application) => {
+const loader = async (app: express.Application) => {
   expressLoader(app);
+
+  await mongooseLoader();
 };
 
 export default loader;
