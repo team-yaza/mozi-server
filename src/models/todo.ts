bbf494179be36f5ddd9142ea08898e2426ab59f0
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-07-10 21:01:37 +0900

    feat: create todo table (#15)

---
+++ src/models/todo.ts
@@ -0,0 +1,16 @@
+import mongoose from 'mongoose';
+
+const todoSchema = new mongoose.Schema({
+  title: {
+    type: String,
+  },
+
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Todo = mongoose.model('Todo', todoSchema);
+
+export default Todo;
