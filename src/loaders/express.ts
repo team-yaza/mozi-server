371e50372a82fea536da43dc24542686b7466e5f
hyunjin <leehj0110@kakao.com>
2022-07-10 18:36:39 +0900

    feat: cors 문제 해결

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,9 +1,17 @@
 import express from 'express';
+import cors from 'cors';
+
 import routes from '@/routes';
+
+const options: cors.CorsOptions = {
+  origin: ['http://localhost:3000'],
+  credentials: true,
+};
 
 const expressLoader = (app: express.Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+  app.use(cors(options));
 
   app.use('/api', routes);
 };
