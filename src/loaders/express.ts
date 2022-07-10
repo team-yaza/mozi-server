290cb2f9cea327e766c3f9396473f1cbd1d85173
hyunjin <leehj0110@kakao.com>
2022-07-10 14:11:22 +0900

    feat: routes express Application에 등록

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,8 +1,11 @@
 import express from 'express';
+import routes from '@/routes';
 
 const expressLoader = (app: express.Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+
+  app.use('/api', routes);
 };
 
 export default expressLoader;
