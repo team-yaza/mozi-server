b72f7b08264cba77a28a4f0730be28cb7c95e8d1
hyunjin <leehj0110@kakao.com>
2022-07-10 20:03:07 +0900

    refactor: project 스키마에 index 컬럼 추가

--- src/models/project.ts
+++ src/models/project.ts
@@ -7,6 +7,9 @@ const projectSchema = new mongoose.Schema({
   description: {
     type: String,
   },
+  index: {
+    type: Number,
+  },
   headers: {
     type: [{ headerId: mongoose.Schema.Types.ObjectId, title: String, index: Number }],
     default: [],
