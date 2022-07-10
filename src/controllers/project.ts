4f17f5ae1ac33f08f6e2f13760cf4b01fd1b520a
hyunjin <leehj0110@kakao.com>
2022-07-10 14:22:13 +0900

    feat: createProjectHeaderHandler

--- src/controllers/project.ts
+++ src/controllers/project.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 
-import { findAllProjects, createProject } from '@/services/project';
+import { findAllProjects, createProject, findProjectById } from '@/services/project';
+import { createHeader } from '@/services/header';
 
 export const getAllProjectsHandler = async (req: Request, res: Response) => {
   try {
@@ -19,5 +20,23 @@ export const createProjectHandler = async (req: Request, res: Response) => {
     return res.status(201).json(project);
   } catch (error) {
     res.status(400).json({ message: 'Project not created' });
+  }
+};
+
+export const createProjectHeaderHandler = async (req: Request, res: Response) => {
+  try {
+    const headerTitle = req.body.header.title;
+    const projectId = req.body.projectId;
+
+    const header = await createHeader(headerTitle);
+    const project = await findProjectById(projectId);
+
+    project?.headers?.push(header); // TODO mongoose Model Type 정의 후 refactor
+
+    await project?.save();
+
+    return res.status(201).json({ message: 'Header created in Project' });
+  } catch (error) {
+    res.status(400).json({ message: 'Header not created' });
   }
 };
