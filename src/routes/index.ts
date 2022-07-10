f8bb8238a6de86666f85af8a421579e316580672
hyunjin <leehj0110@kakao.com>
2022-07-10 14:04:25 +0900

    feat: header router 추가

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
 import todoRouter from '@/routes/todo';
+import headerRouter from '@/routes/header';
 import projectRouter from '@/routes/project';
 
 const routes = express.Router();
 
 routes.use('/todo', todoRouter);
+routes.use('/header', headerRouter);
 routes.use('/project', projectRouter);
 
 export default routes;
