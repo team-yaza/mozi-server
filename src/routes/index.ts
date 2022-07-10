a929cfb5b127a349df754f22ba53a051a4494374
hyunjin <leehj0110@kakao.com>
2022-07-10 19:00:20 +0900

    feat: add area route

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,12 +1,14 @@
 import express from 'express';
 
 import todoRouter from '@/routes/todo';
+import areaRouter from '@/routes/area';
 import headerRouter from '@/routes/header';
 import projectRouter from '@/routes/project';
 
 const routes = express.Router();
 
 routes.use('/todo', todoRouter);
+routes.use('/area', areaRouter);
 routes.use('/header', headerRouter);
 routes.use('/project', projectRouter);
 
