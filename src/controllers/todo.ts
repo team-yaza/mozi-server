897e2d0c77bddc214708f9564b2a071da992a98b
sion-k <44183313+sion-k@users.noreply.github.com>
2022-07-12 16:17:44 +0900

    feat:데이터베이스에서 요청된 할 일 삭제 (#22)
    * feat: delete todo service
    
    * feat: delete todo controller
    
    * feat: delete todo route

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -1,6 +1,6 @@
 import { Request, Response } from 'express';
 
-import { findAllTodos, createTodo } from '@/services/todo';
+import { findAllTodos, createTodo, deleteTodo } from '@/services/todo';
 
 export const getAllTodosHandler = async (req: Request, res: Response) => {
   try {
@@ -19,5 +19,16 @@ export const createTodoHandler = async (req: Request, res: Response) => {
     return res.status(200).json(todo);
   } catch (error) {
     res.status(404).json({ message: 'Todo not created' });
+  }
+};
+
+export const deleteTodoHandler = async (req: Request, res: Response) => {
+  try {
+    const result = await deleteTodo(req.query['todoId']);
+
+    if (result) return res.status(200).json({ message: 'remove complete' });
+    else throw 'Todo not found';
+  } catch (error) {
+    res.status(404).json({ message: error });
   }
 };
--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
-import { getAllTodosHandler, createTodoHandler } from '@/controllers/todo';
+import { getAllTodosHandler, createTodoHandler, deleteTodoHandler } from '@/controllers/todo';
 
 const todoRouter = express.Router();
 
 todoRouter.get('/all', getAllTodosHandler);
 todoRouter.post('/create', createTodoHandler);
+todoRouter.delete('/', deleteTodoHandler);
 
 export default todoRouter;
