5bf33dbc1ec9a39187936f0393dec59632dab935
hyunjin <leehj0110@kakao.com>
2022-07-12 12:30:35 +0900

    fix: cors 에러

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -1,10 +1,18 @@
 import express from 'express';
+import cors from 'cors';
 
 import routes from '@/routes';
+
+const options: cors.CorsOptions = {
+  origin: 'http://localhost:3000',
+  credentials: true,
+};
 
 const expressLoader = (app: express.Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: false }));
+
+  app.use(cors(options));
 
   app.use('/api/v1', routes);
 };
