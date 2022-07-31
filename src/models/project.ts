77c7e5a13a05770298570f701718977268fb7d47
hyunjin <leehj0110@kakao.com>
2022-07-31 18:49:10 +0900

    refactor: model에 timestamp 추가

--- src/models/project.ts
+++ src/models/project.ts
@@ -1,25 +1,26 @@
 import mongoose from 'mongoose';
 
-const projectSchema = new mongoose.Schema({
-  title: {
-    type: String,
+const projectSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    index: {
+      type: Number,
+    },
+    headers: {
+      type: [{ headerId: mongoose.Schema.Types.ObjectId, title: String, index: Number }],
+      default: [],
+      ref: 'Header',
+    },
   },
-  description: {
-    type: String,
+  {
+    timestamps: true,
   },
-  index: {
-    type: Number,
-  },
-  headers: {
-    type: [{ headerId: mongoose.Schema.Types.ObjectId, title: String, index: Number }],
-    default: [],
-    ref: 'Header',
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+);
 
 const Project = mongoose.model('Project', projectSchema);
 
