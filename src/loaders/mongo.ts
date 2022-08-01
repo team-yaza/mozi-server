0e2c23ca649f70191accad2540a683444d184210
hyunjin <leehj0110@kakao.com>
2022-08-01 13:50:38 +0900

    feat: mongoose connection err 추가

--- src/loaders/mongo.ts
+++ src/loaders/mongo.ts
@@ -3,6 +3,13 @@ import mongoose from 'mongoose';
 const mongooseLoader = async () => {
   await mongoose.connect('mongodb://127.0.0.1:27017/mozi');
   console.log('mongodb connected');
+
+  mongoose.connection.on('error', (error) => {
+    console.error(error);
+    console.log('MongoDB connection error. Please make sure MongoDB is running.');
+
+    process.exit();
+  });
 };
 
 export default mongooseLoader;
