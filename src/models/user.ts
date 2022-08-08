20fe4bec6922cf103229498891c13ee6138a0e8f
sion-k <axiom0510@gmail.com>
2022-08-08 14:30:25 +0900

    feat: User schema added

---
+++ src/models/user.ts
@@ -0,0 +1,11 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
