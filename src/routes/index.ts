551daaafccc390171e6f8e6b42e2da4ce9592445
sion-k <axiom0510@gmail.com>
2022-08-09 15:06:30 +0900

    feat: auth router added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -5,6 +5,7 @@ import areaRouter from '@/routes/area';
 import headerRouter from '@/routes/header';
 import projectRouter from '@/routes/project';
 import webpushRouter from '@/routes/webpush';
+import authRouter from '@/routes/auth';
 
 const routes = express.Router();
 
@@ -13,5 +14,6 @@ routes.use('/area', areaRouter);
 routes.use('/header', headerRouter);
 routes.use('/project', projectRouter);
 routes.use('/webpush', webpushRouter);
+routes.use('/auth', authRouter);
 
 export default routes;
