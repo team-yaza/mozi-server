cbea9dfdbee7d25f89ab73113fd843f6ee56b640
sion-k <axiom0510@gmail.com>
2022-08-12 17:33:27 +0900

    feat: notification handler added

---
+++ src/controllers/webpush.ts
@@ -0,0 +1,23 @@
+import { Request, Response } from 'express';
+import { findTodo } from '@/services/todo';
+import webpush from 'web-push';
+
+export const notificationHandler = async (req: Request, res: Response) => {
+  const todo = await findTodo(req.params.id);
+
+  const payload = JSON.stringify(todo);
+  const subscription = req.body;
+
+  try {
+    const webpushResponse = await webpush.sendNotification(subscription, payload);
+    res.writeHead(webpushResponse.statusCode, webpushResponse.headers).end(webpushResponse.body);
+  } catch (err: any) {
+    if ('statusCode' in err) {
+      res.writeHead(err.statusCode, err.headers).end(err.body);
+    } else {
+      console.error('err : ', err);
+      res.statusCode = 500;
+      res.end();
+    }
+  }
+};
