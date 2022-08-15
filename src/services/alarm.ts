6daa1312b6d03a1ea17383507cd21a648205663d
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-08-15 22:10:27 +0900

    feat: 알람 삭제 서비스 로직 구현

--- src/services/alarm.ts
+++ src/services/alarm.ts
@@ -3,3 +3,10 @@ import Alarm from '@/models/alarm';
 export const createAlarm = async (userId: any, todoId: any) => {
   await Alarm.create({ userId, todoId });
 };
+
+export const deleteAlarm = async (id: any) => {
+  const result = await Alarm.findOneAndDelete({
+    todoId: id,
+  });
+  return result;
+};
