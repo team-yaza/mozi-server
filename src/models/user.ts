c44e55e963acc84122bb96817f5200b8f72968e8
sion-k <axiom0510@gmail.com>
2022-08-16 17:26:08 +0900

    feat: passport-local-mongoose module added

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
+import passportLocalMongoose from 'passport-local-mongoose';
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
   },
+});
+
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: 'email',
 });
 
 const User = mongoose.model('User', userSchema);
