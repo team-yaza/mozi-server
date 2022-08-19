22fd3f87b4b3a3ceae063db13772712ecce808eb
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-08-19 21:08:48 +0900

    feat: alarm data populate 후 전송

--- src/controllers/alarm.ts
+++ src/controllers/alarm.ts
@@ -5,5 +5,6 @@ import { findAllAlarms } from '@/services/alarm';
 export const getAllAlarmHandler = async (req: Request, res: Response) => {
   const userId = '313233343536373839303131'; // JWT에서 유저 아이디 가져오기
   const alarms = await findAllAlarms(userId);
+
   res.status(200).json(alarms);
 };
--- src/models/alarm.ts
+++ src/models/alarm.ts
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const alarmSchema = new mongoose.Schema({
   todoId: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: 'Todo',
   },
   userId: {
     type: mongoose.Schema.Types.ObjectId,
