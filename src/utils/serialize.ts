5f1044f782b41f52ee3b1432fa201c76e7110fb8
sion-k <44183313+sion-k@users.noreply.github.com>
2022-08-21 19:49:39 +0900

    feat: 네이버 지도 api를 이용해서 좌표를 주소명으로 변환하기 (#79)
    * feat: get address name by coordinates
    
    * feat: error handling
    
    * chore: directory moved to utils
    
    * feat: serialize GeoJson
    
    * chore: xml2js module removed
    
    * chore: directory moved to utils

--- src/utils/serialize.ts
+++ src/utils/serialize.ts
@@ -1,33 +1,9 @@
-import axios from 'axios';
-import xml2js from 'xml2js';
+import { getAddress } from '@/utils/reverseGeocoding';
 
 export const serializeGeoJson = async (longitude: number, latitude: number) => {
-  const xml = await axios.get(
-    `https://naveropenapi.apigw.ntruss.com/map-reversegeocode/v2/gc?coords=${longitude},${latitude}`,
-    {
-      headers: {
-        'X-NCP-APIGW-API-KEY-ID': `${process.env.NAVER_CLIENT_ID}`,
-        'X-NCP-APIGW-API-KEY': `${process.env.NAVER_SECRET_KEY}`,
-      },
-    },
-  );
-
-  //xml to object
-  const parseXml = async (xmlString: string) =>
-    await new Promise((resolve, reject) =>
-      xml2js.parseString(xmlString, (err, jsonData) => {
-        if (err) {
-          reject(err);
-        }
-        resolve(jsonData);
-      }),
-    );
-
-  const name: any = await parseXml(xml.data);
-
   return {
     type: 'Point',
-    name: '충남대학교',
+    name: await getAddress(longitude, latitude),
     coordinates: [longitude, latitude],
   };
 };
