8239f5bf94001d9d248f0deff3aa85b76fea5c2a
hyunjin <leehj0110@kakao.com>
2022-08-25 10:52:42 +0900

    feat: pino logger

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import webpush from 'web-push';
 
 import loader from '@/loaders';
+import logger from '@/utils/logger';
 
 const publicVapidKey = 'BHCoqzR03UrjuAFGPoTDB5t6o05z5K3EYJ1cuZVj9sPF6FxNsS-b7y4ClNaS11L9EUpmT-wUyeZAivwGbkwMAjY';
 const privateVapidKey = 'QNapwA1rlszq7UdCqLo5s5aORi5jAAlCEFMEfZaw4tU';
@@ -13,7 +14,7 @@ const startSever = () => {
 
   loader(app);
 
-  app.listen(3001, () => console.log('server started'));
+  app.listen(3001, () => logger.info('server started'));
 };
 
 startSever();
--- src/loaders/mongo.ts
+++ src/loaders/mongo.ts
@@ -1,9 +1,10 @@
 import mongoose from 'mongoose';
 import config from '@/config';
+import logger from '@/utils/logger';
 
 const mongooseLoader = async () => {
   await mongoose.connect(process.env.NODE_ENV === 'development' ? 'mongodb://127.0.0.1:27017/mozi' : config.mongo);
-  console.log('mongodb connected');
+  logger.info('mongodb connected');
 
   mongoose.connection.on('error', (error) => {
     console.error(error);
