be583fa084b1b56093081c0432ef3d461a8686a8
sion-k <ygonepiece@naver.com>
2022-09-04 18:00:11 +0900

    feat: Auth controllers

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,48 +1,58 @@
 import { Request, Response, NextFunction } from 'express';
+import User from '@/models/user';
+import bcrypt from 'bcrypt';
 import passport from 'passport';
-import { register } from '@/services/auth';
 
-export const registerHandler = async (req: Request, res: Response) => {
-  const { username, password } = req.body;
+export const registerHandler = async (req: Request, res: Response, next: NextFunction) => {
+  const { email, password } = req.body;
+  try {
+    const exUser = await User.findOne({ where: { email } });
+    if (exUser) {
+      console.log('이미 가입된 이메일입니다.');
+      return res.redirect('/login');
+    }
 
-  const user = await register(username, password);
-
-  if (!user) {
-    return res.status(400).send(`${username} already exists.`);
+    const hash = await bcrypt.hash(password, 12);
+    await User.create({
+      email,
+      password: hash,
+    });
+    return res.redirect('/');
+  } catch (error) {
+    console.error(error);
+    return next(error);
   }
-
-  res.status(201).json(user);
 };
 
 export const loginHandler = (req: Request, res: Response, next: NextFunction) => {
-  passport.authenticate('local', (error, user) => {
-    if (error) {
-      console.error(error);
-      return next(error);
+  passport.authenticate('local', (authError, user) => {
+    if (authError) {
+      console.error(authError);
+      next(authError);
     }
     if (!user) {
-      return res.send('user authentication failed');
+      return res.redirect('/');
     }
-    req.login(user, (error) => {
-      if (error) {
-        console.error(error);
-        next(error);
-      } else {
-        res.redirect('/');
+    console.log(`${user} login 성공`);
+    req.login(user, (loginError) => {
+      if (loginError) {
+        console.error(loginError);
+        next(loginError);
       }
+      res.redirect('/');
     });
   })(req, res, next);
 };
 
 export const logoutHandler = (req: Request, res: Response) => {
-  req.logout({}, (err) => {
-    if (err) {
-      console.error(err);
+  req.logout({}, (logoutError) => {
+    if (logoutError) {
+      console.error(logoutError);
     }
   });
-  req.session.destroy((err) => {
-    if (err) {
-      console.error(err);
+  req.session.destroy((logoutError) => {
+    if (logoutError) {
+      console.error(logoutError);
     }
   });
   res.redirect('/');
