b9f2e3b322832f6214e26ecc6375fc9432812292
sion-k <ygonepiece@naver.com>
2022-09-04 17:52:39 +0900

    feat: Todo controllers

--- src/controllers/todo.ts
+++ src/controllers/todo.ts
@@ -1,8 +1,7 @@
 import { Request, Response } from 'express';
-import mongoose from 'mongoose';
 
 import { findAllTodos, createTodo, deleteTodo, updateTodo, findTodo } from '@/services/todo';
-import { createAlarm, deleteAlarm } from '@/services/alarm';
+// import { createAlarm, deleteAlarm } from '@/services/alarm';
 
 export const getAllTodosHandler = async (req: Request, res: Response) => {
   const todos = await findAllTodos();
@@ -23,22 +22,24 @@ export const createTodoHandler = async (req: Request, res: Response) => {
 
 export const deleteTodoHandler = async (req: Request, res: Response) => {
   const result = await deleteTodo(req.params.id);
-  await deleteAlarm(req.params.id);
+  // await deleteAlarm(req.params.id);
 
   if (result) res.status(200).json(result);
   else throw 'Todo was not found';
 };
 
 export const updateTodoHandler = async (req: Request, res: Response) => {
-  const { result, changeLocationFlag } = await updateTodo(req.params.id, req.body);
+  const { affectedCount } = await updateTodo(req.params.id, req.body);
 
-  const location: any = result?.location;
+  const todo = await findTodo(req.params.id);
+
+  // const location: any = (todo as any).location;
 
   // 첫번째 오브젝트 아이디는 유저 정보를 담고있어야함.
-  if (location.name && changeLocationFlag) {
-    await createAlarm(new mongoose.Types.ObjectId('123456789011'), req.params.id);
-  }
+  // if (location.name && changeLocationFlag) {
+  //   await createAlarm(new mongoose.Types.ObjectId('123456789011'), req.params.id);
+  // }
 
-  if (result) res.status(201).json(result);
+  if (affectedCount) res.status(201).json(todo);
   else throw 'Todo was not updated';
 };
