099189c5cda5b443af142cf8721d61cc8fc55353
sion-k <ygonepiece@naver.com>
2022-09-04 17:55:38 +0900

    feat: Passport loader

--- src/loaders/passport.ts
+++ src/loaders/passport.ts
@@ -2,19 +2,26 @@ import express from 'express';
 import passport from 'passport';
 import User from '@/models/user';
 
+import localStrategy from '@/utils/localStrategy';
 import kakaoStrategy from '@/utils/kakaoStrategy';
 
-const passportLoader = (app: express.Application) => {
+export const passportLoader = (app: express.Application) => {
   app.use(passport.initialize());
   app.use(passport.session());
 
-  passport.use(User.createStrategy() as any);
+  passport.serializeUser((user: any, done) => {
+    done(null, user.id);
+  });
 
-  passport.serializeUser(User.serializeUser() as any);
+  passport.deserializeUser(async (id, done) => {
+    const user = await User.findOne({
+      where: {
+        id,
+      },
+    });
+    done(null, user);
+  });
 
-  passport.deserializeUser(User.deserializeUser());
-
+  passport.use(localStrategy);
   passport.use(kakaoStrategy);
 };
-
-export default passportLoader;
