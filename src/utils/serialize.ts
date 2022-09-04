014c6cb5fb45daa6f17dc9b2f0dad8b5af4bd7b4
sion-k <ygonepiece@naver.com>
2022-09-04 17:54:27 +0900

    chore: Serialize geoJSON removed

--- src/utils/serialize.ts
+++
@@ -1,9 +0,0 @@
-import { getAddress } from '@/utils/reverseGeocoding';
-
-export const serializeGeoJson = async (longitude: number, latitude: number) => {
-  return {
-    type: 'Point',
-    name: await getAddress(longitude, latitude),
-    coordinates: [longitude, latitude],
-  };
-};
