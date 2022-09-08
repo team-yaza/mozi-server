1efeaabbf8520d1989257f4e25a7adada53b3262
sion-k <axiom0510@gmail.com>
2022-09-08 22:06:16 +0900

    fix: alarmed feature removed

--- src/controllers/webpush.ts
+++ src/controllers/webpush.ts
@@ -1,16 +1,22 @@
 import { Request, Response } from 'express';
 import { notifyTodo } from '@/services/webpush';
-import { updateTodoAlarmed } from '@/services/todo';
+import { updateTodo } from '@/services/todo';
 
 export const notificationHandler = async (req: Request, res: Response) => {
   const subscription = JSON.parse(req.body.subscription);
   const webpushResponse = await notifyTodo(subscription, req.params.id);
-  await updateTodoAlarmed(req.params.id, true);
+
+  await updateTodo(req.params.id, {
+    alarmed: true,
+  });
+
   res.writeHead(webpushResponse.statusCode, webpushResponse.headers);
   res.end(webpushResponse.body);
 };
 
 export const setNotificationHandler = async (req: Request, res: Response) => {
-  await updateTodoAlarmed(req.params.id, false);
+  await updateTodo(req.params.id, {
+    alarmed: false,
+  });
   res.status(200).send();
 };
