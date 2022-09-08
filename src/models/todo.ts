ea35a6964e6623dbe51e9e6c73cc57b35fc60c83
sion-k <axiom0510@gmail.com>
2022-09-08 22:05:33 +0900

    fix: model definition by extends

--- src/models/todo.ts
+++ src/models/todo.ts
@@ -1,13 +1,41 @@
+import { Model, InferAttributes, InferCreationAttributes, CreationOptional, DataTypes } from 'sequelize';
 import sequelize from '@/models';
-import { DataTypes } from 'sequelize';
 
-const Todo = sequelize.define('todo', {
-  title: DataTypes.STRING,
-  description: DataTypes.STRING,
-  done: DataTypes.BOOLEAN,
-  alarmed: DataTypes.BOOLEAN,
-  location: DataTypes.GEOMETRY,
-});
+class Todo extends Model<InferAttributes<Todo>, InferCreationAttributes<Todo>> {
+  declare id: CreationOptional<number>;
+  declare title: string;
+  declare description: string;
+  declare done?: boolean;
+  declare alarmed?: boolean;
+  declare longitude?: CreationOptional<number>;
+  declare latitude?: CreationOptional<number>;
+}
+
+Todo.init(
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    title: DataTypes.STRING,
+    description: DataTypes.STRING,
+    done: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    alarmed: {
+      type: DataTypes.BOOLEAN,
+      defaultValue: false,
+    },
+    longitude: DataTypes.FLOAT,
+    latitude: DataTypes.FLOAT,
+  },
+  {
+    tableName: 'todos',
+    sequelize,
+  },
+);
 
 (async () => {
   await Todo.sync();
