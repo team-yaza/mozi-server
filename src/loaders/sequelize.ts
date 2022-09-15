2f7cc3ad9cd70c91ed9a16af5b411c4d0b22c9e3
sion-k <axiom0510@gmail.com>
2022-09-15 20:18:30 +0900

    feat: Model association load

--- src/loaders/sequelize.ts
+++ src/loaders/sequelize.ts
@@ -1,8 +1,10 @@
 import mysql from 'mysql2/promise';
+import modelInit from '@/models';
 
 const sequelizeLoader = async () => {
   const connection = await mysql.createConnection('mysql://root:root@localhost:3306');
   await connection.query('CREATE DATABASE IF NOT EXISTS mozi;');
+  await modelInit();
 };
 
 export default sequelizeLoader;
