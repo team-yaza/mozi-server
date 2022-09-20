d49d8da923cc9074d2c7f795fc4f2530c84dbbaf
sion-k <axiom0510@gmail.com>
2022-09-20 22:29:30 +0900

    chore: type removed

--- src/types/user.ts
+++
@@ -1,9 +0,0 @@
-export interface User {
-  id: string;
-  name: string;
-  email?: string;
-  password?: string;
-  image?: string;
-  thumbnail_image_url?: string;
-  profile_image_url?: string;
-}
