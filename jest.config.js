80830b056369b466d7abd09b97eeafafd5713987
sion-k <axiom0510@gmail.com>
2022-10-03 14:26:00 +0900

    chore: setting

--- jest.config.js
+++ jest.config.js
@@ -7,17 +7,15 @@ const moduleNameMapper = {}; //jest환경에서도 module Alias를 사용하기 
 
 for (const key in paths) {
   const moduleName = `${key.slice(0, key.length - 1)}(.*)$`;
-  moduleNameMapper[moduleName] = resolve(__dirname, `./src/${paths[key][0].slice(0, paths[key][0].length - 1)}$1`);
+  moduleNameMapper[moduleName] = resolve(__dirname, `./${paths[key][0].slice(0, paths[key][0].length - 1)}$1`);
 }
 
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node', //jest를 node 환경에서 돌리기 위해선 testEnvironment 설정이 필요합니다.
-  roots: ['<rootDir>/src'],
   setupFiles: ['dotenv/config'],
   transform: { '^.+\\.tsx?$': 'ts-jest' },
-  testRegex: '(/tests/.*|(\\.|/)(test|spec))\\.tsx?$',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  setupFilesAfterEnv: ['<rootDir>/tests/setup.ts'], //테스트가 돌아가기 전에 의존성이나 추가 환경을 구성하기위한 setup file입니다.
   moduleNameMapper,
-  // setupFilesAfterEnv: ['<rootDir>/src/tests/setup.ts'], //테스트가 돌아가기 전에 의존성이나 추가 환경을 구성하기위한 setup file입니다.
 };
