56d52306012629306d41775c5b146df55d06dad4
sion-k <axiom0510@gmail.com>
2022-10-06 18:07:56 +0900

    fix: error message format

--- src/middlewares/todo.ts
+++ src/middlewares/todo.ts
@@ -3,12 +3,14 @@ import { NextFunction, Request, Response } from 'express';
 import { z } from 'zod';
 
 export const uuidHandler = (req: Request, res: Response, next: NextFunction) => {
-  const uuidSchema = z.string().uuid('Invalid uuid');
+  const uuidSchema = z.string().uuid();
 
   try {
     uuidSchema.parse(req.params.id);
   } catch (error) {
-    return res.status(400).json(error);
+    return res.status(400).json({
+      message: 'Invalid uuid',
+    });
   }
 
   return next();
