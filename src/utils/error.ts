f82b66ca51e0c51de5a4b370aaecd62b50b262a5
sion-k <axiom0510@gmail.com>
2022-10-09 15:17:57 +0900

    fix: invalid uuid error removed

--- src/utils/error.ts
+++ src/utils/error.ts
@@ -1,5 +1,3 @@
 import createHttpError from 'http-errors';
 
 export const todoNotFound = new createHttpError.NotFound('Todo not found');
-
-export const invalidUUID = new createHttpError.BadRequest('Invalid todo UUID');
