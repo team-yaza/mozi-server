2862f88a8d88bc8a39a3d1a9398da90600770aa6
sion-k <axiom0510@gmail.com>
2022-10-12 17:18:34 +0900

    fix: what if...?

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -6,48 +6,42 @@ import config from '@/config';
 import { getProfile } from '@/services/kakao';
 
 export const kakaoHandler = async (req: Request, res: Response) => {
-  try {
-    const { accessToken } = req.body;
+  const { accessToken } = req.body;
 
-    const result = await getProfile(accessToken);
+  const result = await getProfile(accessToken);
 
-    const { id, email } = result;
+  const { id, email } = result;
 
-    const {
-      nickname: name,
-      profile_image_url: profileImage,
-      thumbnail_image_url: thumbnailImage,
-    } = result.kakao_account.profile;
+  const {
+    nickname: name,
+    profile_image_url: profileImage,
+    thumbnail_image_url: thumbnailImage,
+  } = result.kakao_account.profile;
 
-    const existedUser: any = await User.findOne({ where: { id } });
+  const existedUser: any = await User.findOne({ where: { id } });
 
-    if (existedUser) {
-      await existedUser.update({ email, name, profileImage, thumbnailImage });
-      await existedUser.save();
-    } else {
-      await User.create({
-        id,
-        email,
-        name,
-        profileImage,
-        thumbnailImage,
-      });
-    }
-
-    const token = jwt.sign(
-      {
-        id,
-        name,
-        email,
-        profileImage,
-      },
-      config.jwtSecret,
-      { issuer: 'hyunjin' },
-    );
-    res.status(200).json(token);
-    // res.status(200).cookie('token', token, { secure: true, sameSite: 'none', path: '/' }).json(token);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ message: '카카오 로그인에 실패했습니다.' });
+  if (existedUser) {
+    await existedUser.update({ email, name, profileImage, thumbnailImage });
+    await existedUser.save();
+  } else {
+    await User.create({
+      id,
+      email,
+      name,
+      profileImage,
+      thumbnailImage,
+    });
   }
+
+  const token = jwt.sign(
+    {
+      id,
+      name,
+      email,
+      profileImage,
+    },
+    config.jwtSecret,
+    { issuer: 'hyunjin' },
+  );
+  res.status(200).json(token);
 };
--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,9 @@
 import express from 'express';
 import { kakaoHandler } from '@/controllers/auth';
+import asyncHandler from '@/utils/asyncHandler';
 
 const authRouter = express.Router();
 
-authRouter.post('/kakao', kakaoHandler);
+authRouter.post('/kakao', asyncHandler(kakaoHandler));
 
 export default authRouter;
