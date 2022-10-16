b548425f9a99e5bd8ec80a6a998d6a4d6ed368f6
Sion Kim <44183313+sion-k@users.noreply.github.com>
2022-10-16 20:54:57 +0900

    feat: Prometheus client middleware (#134)
    * feat: prom module added
    
    * feat: prom loader added
    
    * feat: prom loader added
    
    * fix: server instance

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -3,11 +3,12 @@ import express from 'express';
 import expressLoader from '@/loaders/express';
 import { routesLoader } from './routes';
 import sequelizeLoader from './sequelize';
+import promLoader from './prom';
 
 const loader = async (app: express.Application) => {
   expressLoader(app);
   await sequelizeLoader();
-
+  promLoader(app);
   routesLoader(app);
 };
 
