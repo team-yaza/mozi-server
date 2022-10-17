b38be4dfee942ec707043011a726593847d19909
HYUNJIN LEE <63354527+hyunjinee@users.noreply.github.com>
2022-10-17 16:33:10 +0900

    feat: sequelize sync option (#138)

--- src/loaders/sequelize.ts
+++ src/loaders/sequelize.ts
@@ -1,14 +1,7 @@
-import mysql from 'mysql2/promise';
 import modelInit from '@/models';
 
 const sequelizeLoader = async () => {
   const host = process.env.NODE_ENV === 'development' ? 'localhost' : 'db';
-  const connection = await mysql.createConnection({
-    host,
-    user: 'root',
-    password: 'root',
-  });
-  await connection.query('CREATE DATABASE IF NOT EXISTS mozi;');
   await modelInit();
 };
 
