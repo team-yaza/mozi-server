d6e3b112f8840292878748d3e9af2c87c062d2e1
sion-k <axiom0510@gmail.com>
2022-10-24 14:23:26 +0900

    feat: location search req body validation

--- src/routes/location.ts
+++ src/routes/location.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import { instantSearchHandler, getNearbyHandler, getTodosWithLocationsHandler } from '@/controllers/location';
+import { locationValidator } from '@/middlewares/location';
 
 const locationRouter = express.Router();
 
 locationRouter.get('/', getTodosWithLocationsHandler);
-locationRouter.post('/nearby', instantSearchHandler, getNearbyHandler);
+locationRouter.post('/nearby', locationValidator, instantSearchHandler, getNearbyHandler);
 
 export default locationRouter;
