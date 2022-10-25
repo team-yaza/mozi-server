142e383b3614f34d9fd98c7ba5feba401a0fd0df
HanCiHu <chanhui.yu.cnu@gmail.com>
2022-10-25 15:25:18 +0900

    refactor: 3 Tier Architecture로 업그레이드를 위한 리팩토링

--- src/loaders/sequelize.ts
+++ src/loaders/sequelize.ts
@@ -3,19 +3,22 @@ import modelInit from '@/models';
 import { Sequelize } from 'sequelize';
 
 const sequelizeLoader = async () => {
-  const host = process.env.NODE_ENV === 'development' ? 'localhost' : 'db';
+  const host = process.env.NODE_ENV === 'development' ? 'localhost' : process.env.DATABASE_URL;
 
   const connection = await mysql.createConnection({
     host,
-    user: 'root',
-    password: 'root',
+    user: process.env.MYSQL_USER,
+    password: process.env.MYSQL_PASSWORD,
   });
 
   await connection.query('CREATE DATABASE IF NOT EXISTS mozi;');
 
-  const sequelize = new Sequelize(`mysql://root:root@${host}:3306/mozi`, {
-    logging: false,
-  });
+  const sequelize = new Sequelize(
+    `mysql://${process.env.MYSQL_USER}:${process.env.MYSQL_PASSWORD}@${host}:${process.env.DATABASE_PORT}/${process.env.DATABASE_NAME}`,
+    {
+      logging: false,
+    },
+  );
 
   await modelInit(sequelize);
 };
