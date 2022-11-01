84c3237fd7355d37fbc7b0973ccfd97cbd78cb35
sion-k <ygonepiece@naver.com>
2022-11-01 22:35:45 +0900

    fix: bearerAuth refactoring

--- src/todos/todosController.ts
+++ src/todos/todosController.ts
@@ -4,13 +4,13 @@ import { TodoCreationParams } from './todo';
 import { TodosService } from './todosService';
 
 @Route('todos')
+@Security('bearerAuth')
 export class TodosController extends Controller {
   /**
    * 사용자의 모든 Todo를 가져옵니다.
    * @summary 사용자의 모든 Todo를 가져옵니다.
    */
   @SuccessResponse('200', 'Ok')
-  @Security('bearerAuth')
   @Get()
   public async getTodos(@Request() req: express.Request) {
     return await new TodosService().getAll(req.user);
@@ -21,7 +21,6 @@ export class TodosController extends Controller {
    * @summary Todo를 DB에 생성하고 가져옵니다.
    */
   @SuccessResponse('201', 'Created')
-  @Security('bearerAuth')
   @Post()
   public async createTodo(@Request() req: express.Request, @Body() reqBody: TodoCreationParams) {
     return await new TodosService().create(req.user, reqBody);
@@ -33,7 +32,6 @@ export class TodosController extends Controller {
    * @summary 사용자의 Todo를 가져옵니다.
    */
   @SuccessResponse('200', 'Ok')
-  @Security('bearerAuth')
   @Get('{id}')
   public async getTodo(@Path() id: string, @Request() req: express.Request) {
     return await new TodosService().get(req.user, id);
@@ -45,7 +43,6 @@ export class TodosController extends Controller {
    * @summary 사용자의 Todo를 삭제합니다.
    */
   @SuccessResponse('200', 'Ok')
-  @Security('bearerAuth')
   @Delete('{id}')
   public async removeTodo(@Path() id: string, @Request() req: express.Request) {
     return await new TodosService().remove(req.user, id);
@@ -57,7 +54,6 @@ export class TodosController extends Controller {
    * @summary 사용자의 Todo를 업데이트합니다.
    */
   @SuccessResponse('204', 'No contents')
-  @Security('bearerAuth')
   @Patch('{id}')
   public async updateTodo(@Path() id: string, @Request() req: express.Request, @Body() reqBody: TodoCreationParams) {
     await new TodosService().update(req.user, id, reqBody);
