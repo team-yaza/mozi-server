97ddaa1a5c2c17e1d6c01ab6b7d9e8ee078e81a0
sion-k <axiom0510@gmail.com>
2022-11-04 17:18:17 +0900

    feat: web push

---
+++ src/webpush/webpushService.ts
@@ -0,0 +1,14 @@
+import Todo from '@/models/todo';
+import webpush, { PushSubscription } from 'web-push';
+
+export class webpushService {
+  public async pushTodo(id: string, subscription: PushSubscription) {
+    const todo = await Todo.findByPk(id);
+
+    if (!todo) return;
+
+    await todo.update('alarmed', true);
+
+    return await webpush.sendNotification(subscription, JSON.stringify(todo));
+  }
+}
