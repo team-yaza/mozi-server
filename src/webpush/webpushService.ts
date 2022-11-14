6191af35b80a8be54b28be452d074efd488e81e1
sion-k <axiom0510@gmail.com>
2022-11-14 15:37:14 +0900

    refactor: webpush

--- src/webpush/webpushService.ts
+++ src/webpush/webpushService.ts
@@ -1,7 +1,18 @@
 import { Todo } from '@/todos/todo';
 import webpush, { PushSubscription } from 'web-push';
 
-export class webpushService {
+export class WebpushService {
+  static publicVapidKey = 'BHCoqzR03UrjuAFGPoTDB5t6o05z5K3EYJ1cuZVj9sPF6FxNsS-b7y4ClNaS11L9EUpmT-wUyeZAivwGbkwMAjY';
+  static privateVapidKey = 'QNapwA1rlszq7UdCqLo5s5aORi5jAAlCEFMEfZaw4tU';
+
+  constructor() {
+    webpush.setVapidDetails(
+      'mailto:leehj0110@kakao.com',
+      WebpushService.publicVapidKey,
+      WebpushService.privateVapidKey,
+    );
+  }
+
   public async pushTodo(id: string, subscription: PushSubscription) {
     const todo = await Todo.findByPk(id);
 
