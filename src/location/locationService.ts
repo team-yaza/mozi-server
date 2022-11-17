0d880805827851db6ffe8c147402bc08bed4b8fb
sion-k <axiom0510@gmail.com>
2022-11-17 16:39:39 +0900

    feat: recomended locations

--- src/location/locationService.ts
+++ src/location/locationService.ts
@@ -2,12 +2,16 @@ import axios from 'axios';
 import { location } from './location';
 
 export class LocationService {
-  public async search(longitude: number, latitude: number, keyword: string) {
+  async search(longitude: number, latitude: number, keyword: string) {
     try {
       return await new NaverInstantSearch().search(longitude, latitude, keyword);
     } catch (error: unknown) {
       return await new GoogleNearBySearch().search(longitude, latitude, keyword);
     }
+  }
+
+  async recommend(longitude: number, latitude: number) {
+    return await new NaverRecommended().search(longitude, latitude, '가볼만한 곳');
   }
 }
 
@@ -81,7 +85,7 @@ class GoogleNearBySearch extends LocationSearch {
   protected parse(data: any): location[] {
     const { results } = data;
 
-    const locations: location[] = results((data: any) => {
+    const locations: location[] = results.map((data: any) => {
       const {
         name,
         geometry: {
@@ -92,6 +96,42 @@ class GoogleNearBySearch extends LocationSearch {
       const location: location = {
         name,
         location: [longitude, latitude],
+      };
+
+      return location;
+    });
+
+    return locations;
+  }
+}
+
+class NaverRecommended extends LocationSearch {
+  protected url = 'https://map.naver.com/v5/api/search';
+  protected apiKey: string | undefined;
+
+  protected async request(longitude: number, latitude: number, keyword: string): Promise<any> {
+    const { data } = await axios.get(this.url, {
+      params: {
+        query: keyword,
+        searchCoord: `${longitude};${latitude}`,
+        isPlaceRecommendationReplace: true,
+      },
+    });
+
+    return data;
+  }
+
+  protected parse(data: any): location[] {
+    const {
+      result: {
+        place: { list },
+      },
+    } = data;
+
+    const locations: location[] = list.map((data: any) => {
+      const location: location = {
+        name: data.name,
+        location: [parseFloat(data.x), parseFloat(data.y)],
       };
 
       return location;
